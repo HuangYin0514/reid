1f2501c348ecfbfbee05c2df50c64625203e48f1
HuangYin <1071324110@qq.com>
2021-10-03 17:11:14 +0800

    apnet

--- train.py
+++ train.py
@@ -1,13 +1,19 @@
-
-
 def pcb_script():
     from train_script.baseline_script import train
+
+    print("目标精度94.1%。。。")
+    return train
+
+
+# 参考apnet中的模块
+def baseline_apnet_scrip():
+    from train_script.baseline_apnet_scrip import train
 
     return train
 
 
 if __name__ == "__main__":
 
-    train = pcb_script()
+    train = baseline_apnet_scrip()
     print("运行中。。。" + train.__globals__["__name__"])
     train()
