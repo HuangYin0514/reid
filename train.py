4f3e563c2db70c70d96c70f258982d57199cebfa
HuangYin <1071324110@qq.com>
2021-10-18 16:46:44 +0800

    fix

--- train.py
+++ train.py
@@ -15,13 +15,11 @@ def baseline_apnet_scrip():
 # baseline_apnet_drop_script
 def baseline_apnet_drop_script():
     from train_script.baseline_apnet_drop_script import train
-    # print("目标精度95.0%。。。")
     return train
 
 # baseline_apnet_drop_script
 def baseline_apnet_2branch_script():
     from train_script.baseline_apnet_2branch_script import train
-    # print("目标精度95.0%。。。")
     return train
 
 # pcb
@@ -39,7 +37,7 @@ def pcb_apnet_script():
 
 if __name__ == "__main__":
 
-    train = baseline_apnet_2branch_script()
+    train = baseline_apnet_scrip()
     print("=" * 40)
     print("运行中。。。" + train.__globals__["__name__"])
     print("=" * 40)
