5be3c34aa19228716992f6bcdb31813155c1db1e
HuangYin <1071324110@qq.com>
2021-10-23 17:03:49 +0800

    fix

--- train.py
+++ train.py
@@ -1,44 +1,13 @@
-def pcb_script():
-    from train_script.baseline_script import train
-
-    print("目标精度94.1%。。。")
-    return train
-
-
 # 参考apnet中的模块
 def baseline_apnet_scrip():
     from train_script.baseline_apnet_scrip import train
+
     print("目标精度95.0%。。。")
     return train
 
-# baseline_apnet_drop_script
-def baseline_apnet_drop_script():
-    from train_script.baseline_apnet_drop_script import train
-    print("目标精度94.9%。。。")
-    return train
-
-def baseline_apnet_FPB():
-    from train_script.baseline_apnet_FPB_script import train
-    return train
-
-def baseline_apnet_2branch_script():
-    from train_script.baseline_apnet_2branch_script import train
-    return train
-
-# pcb
-def pcb_script():
-    from train_script.pcb_script import train
-
-    return train
 
 def pcb_ffm_script():
     from train_script.pcb_ffm_script import train
-    return train
-
-
-
-def pcb_apnet_script():
-    from train_script.pcb_apnet_scrip import train
 
     return train
 
